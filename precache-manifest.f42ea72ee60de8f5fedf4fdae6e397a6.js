9371e4dceb777a60d234f2445e83032e62e9613e
Igor <igor.ogerchuk@gmail.com>
2019-11-13 00:25:21 +0200

    Updates

---
+++ precache-manifest.f42ea72ee60de8f5fedf4fdae6e397a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e40fd00108bf3a921a3aa7b54688b9",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "3b011bd74035c06c5003",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.b0a844b8.chunk.css"
+  },
+  {
+    "revision": "ea8c596eb9056cf4423e",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.da468187.chunk.js"
+  },
+  {
+    "revision": "3b011bd74035c06c5003",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.22566eac.chunk.js"
+  },
+  {
+    "revision": "26f980cacd962de52565",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.9d9945f4.js"
+  }
+]);
