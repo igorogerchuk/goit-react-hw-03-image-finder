2ca8d9f9db06ed3cad023f5cf5e33312a9aa7ed1
Igor <igor.ogerchuk@gmail.com>
2019-11-20 00:06:39 +0200

    return App

---
+++ src/components/App/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ImageFinder from '../ImageFinder';
+
+const App = () => {
+  return <ImageFinder />;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ImageFinder from './components/ImageFinder';
+import App from './components/App';
 
-ReactDOM.render(<ImageFinder />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
